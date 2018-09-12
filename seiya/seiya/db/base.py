d1c5c567f7c090b71cb4334bc723d590e333da0a
Manchangdx <1195581533@qq.com>
2018-09-13 00:27:53 +0800

    添加大作业代码框架

---
+++ seiya/seiya/db/base.py
@@ -0,0 +1,6 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+
+engine = create_engine('mysql://root@localhost:3306/seiya?charset=utf8')
+Base = declarative_base(engine)
